65ea431808cd7764622a39965cd1c3b5d32ea692
Filip Bontin <filip.bontin@jayway.com>
2019-03-15 11:55:07 +0100

    server: cleanup

--- server/game.js
+++ server/game.js
@@ -44,103 +44,12 @@ function isAlive(id) {
 }
 
 /**
- * Moves the bullets, and removes them if they are out of bounds
- * @param {{x: number, y: number, direction: Matter.Vector}[]} bullets The current bullets
- * @returns {{x: number, y: number, direction: Matter.Vector}[]} The new bullets
- */
-function moveBullets(bullets) {
-  return bullets
-    .filter(bullet => bullet)
-    .map(bullet => {
-      let newPositionVector = Matter.Vector.create(bullet.x, bullet.y);
-      newPositionVector = Matter.Vector.add(
-        newPositionVector,
-        Matter.Vector.mult(bullet.direction, BULLET_SPEED)
-      );
-
-      // If bullet is out of bounds, remove from state
-      const WIDTH = 600;
-      const HEIGHT = 600;
-      const { x, y } = newPositionVector;
-      const bulletIsOutOfBounds = x > WIDTH || x < 0 || y > HEIGHT || y < 0;
-      return bulletIsOutOfBounds
-        ? null
-        : { ...bullet, x: newPositionVector.x, y: newPositionVector.y };
-    })
-    .filter(b => !!b);
-}
-
-/**
- * Turns the player if the `a` or `d` key has been pressed
- * @returns {number} The rotation of the player, in radians
- */
-function turnPlayer(pressedKeys, state) {
-  if (pressedKeys.a === "keydown") {
-    return state.rotation - ROTATION_SPEED;
-  }
-  if (pressedKeys.d === "keydown") {
-    return state.rotation + ROTATION_SPEED;
-  }
-  return state.rotation;
-}
-
-/**
- * Moves the player if the `w` or `s` key has been pressed
- * @returns {{x: number, y: number}} The new position of the player
- */
-function movePlayer(pressedKeys, state) {
-  if (pressedKeys.w === "keydown" || pressedKeys.s === "keydown") {
-    const speed = pressedKeys.w === "keydown" ? SPEED : -SPEED;
-
-    let translation = Matter.Vector.create(0, -1);
-    translation = Matter.Vector.rotate(translation, state.rotation);
-    translation = Matter.Vector.mult(translation, speed);
-
-    const currentPosition = Matter.Vector.create(state.x, state.y);
-    const nextPosition = Matter.Vector.add(currentPosition, translation);
-
-    return { x: nextPosition.x, y: nextPosition.y };
-  }
-  return { x: state.x, y: state.y };
-}
-
-/**
- * Shoots if space key was released.
- * @returns {void}
- */
-function shoot(pressedKeys, state) {
-  if (pressedKeys.space === "release") {
-    const direction = Matter.Vector.rotate(
-      Matter.Vector.create(0, -1),
-      state.rotation
-    );
-
-    world.bullets.push({
-      x: state.x + direction.x * state.height,
-      y: state.y + direction.y * state.width,
-      radius: 6,
-      direction
-    });
-    pressedKeys.space = "keyup";
-  }
-}
-
-/**
  * Check pressedkeys, update player state
  * @param {{pressedKeys: {}, state: {x: number, y: number, width:number, height: number, rotation: number}}} player
  * @param {*} id
  */
 function updatePlayer(player, id) {
   const { pressedKeys, state } = player;
-
-  state.rotation = turnPlayer(pressedKeys, state);
-
-  const { x, y } = movePlayer(pressedKeys, state);
-  state.x = x;
-  state.y = y;
-
-  shoot(pressedKeys, state);
-
   players.set(id, { ...player, state });
 }
 
@@ -168,9 +77,6 @@ function checkCollisions() {
  */
 function updateState() {
   players.forEach(updatePlayer);
-
-  world.bullets = moveBullets(world.bullets);
-
   checkCollisions();
 
   const nextState = {
